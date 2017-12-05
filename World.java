f74d5f5d60a9775301adb7fedf7c0c69c21eff5d
James Kingan <github@j-kb.net>
2017-12-04 23:53:00 -0500

    Update World.java

--- World.java
+++ World.java
@@ -6,20 +6,22 @@ public class World{
   private char[][] mirrorWorld;
   private int rows;
   private int cols;
+  private int ticks;
 
-  public World(int inRows, int inCols){
+  public World(int inRows, int inCols, int inTicks){
     int rowsPlus = inRows + 2; //Create invisible rows on either side
     int colsPlus = inCols + 2; //Create invisible columns on either side
     rows = inRows;
     cols = inCols;
+    ticks = inTicks;
     world = new char[rowsPlus][colsPlus];
     for (char[] a: world) {
       Arrays.fill(a, ' ');
     }
     fillRandom();
-    tick(10);
-    drawWorld();
-    glider(2,2);
+    tick(ticks);
+    // drawWorld();
+    // glider(2,2);
   }
 
   private void tick(int loops){
@@ -27,51 +29,63 @@ public class World{
     for(int h = 0; h < loops; h++){
       for(int i = 1; i <= rows; i++) {
         for(int j = 1; j <= cols; j++) {
-          // int x = glider(i,j);
+          int x = glider(i,j);
+          // Conway logic
+          if(x < 2)
+            mirrorWorld[i][j] = ' ';
+          if(x > 3)
+            mirrorWorld[i][j] = ' ';
+          if(x == 3)
+            mirrorWorld[i][j] = '*';
           // System.out.println(x);
         }
       }
-      // drawWorld();
+      drawWorld();
+      try{
+        Thread.sleep(500);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
   }
 
   private void fillRandom(){
     for(int i = 1; i <= rows; i++) {
       for(int j = 1; j <= cols; j++) {
-        if((int)(Math.random() * 2) == 0){
+        if((int)(Math.random() * 20) == 0){
           world[i][j] = '*';
-          System.out.println("Filling point " + i + ", " + j);
+          // System.out.println("Filling point " + i + ", " + j);
         }
       }
-      System.out.println();
+      // System.out.println();
     }
   }
 
   private void drawWorld(){
-    System.out.println(" 0123456789");
+    // System.out.println(" 0123456789");
     for(int i = 1; i <= rows; i++) {
-      System.out.print(i - 1);
+      // System.out.print(i - 1);
       for(int j = 1; j <= cols; j++) {
         System.out.print(world[i][j]);
       }
-      System.out.println();
+      System.out.println("\u001b[0m");
     }
   }
 
   private int glider(int x, int y){
     int n=0;
-    System.out.println("Grid:\n_________");
+    // System.out.println("Grid:\n_________");
     for(int i = x - 1; i <= x + 1; i++) {
       for(int j = y - 1; j <= y + 1; j++){
-        System.out.print(world[i][j]);
+        // System.out.print(world[i][j]);
         if(world[i][j] == '*')
           n++;
       }
-      System.out.println();
+      // System.out.println();
     }
     if(world[x][y] == '*')
       n--;
-    System.out.println("Point " + x + ", " + y + " has " + n + " neighbors.");
+    // System.out.println("Point " + x + ", " + y + " has " + n + " neighbors.");
     return n;
   }
 }
