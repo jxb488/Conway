84323a12b54fc85c6d1938690e9e9858bdfbbcd2
James Kingan <github@j-kb.net>
2017-12-05 01:06:02 -0500

    Update World.java

--- World.java
+++ World.java
@@ -5,28 +5,41 @@ public class World{
   private char[][] world;
   private char[][] mirrorWorld;
   private int rows;
+  private int rowsPlus;
   private int cols;
+  private int colsPlus;
   private int ticks;
+  private int randomFactor;
 
-  public World(int inRows, int inCols, int inTicks){
-    int rowsPlus = inRows + 2; //Create invisible rows on either side
-    int colsPlus = inCols + 2; //Create invisible columns on either side
+  public World(int inRows, int inCols, int inTicks, int inRandom){
+    rowsPlus = inRows + 2; //Create invisible rows on either side
+    colsPlus = inCols + 2; //Create invisible columns on either side
     rows = inRows;
     cols = inCols;
     ticks = inTicks;
+    randomFactor = inRandom;
     world = new char[rowsPlus][colsPlus];
     for (char[] a: world) {
       Arrays.fill(a, ' ');
     }
-    fillRandom();
+    // fillRandom();
+    // mirrorWorld = world;
+
+    world[1][2] = '*';
+    world[2][3] = '*';
+    world[3][1] = '*';
+    world[3][2] = '*';
+    world[3][3] = '*';
+
     tick(ticks);
     // drawWorld();
     // glider(2,2);
   }
 
   private void tick(int loops){
-    mirrorWorld = world;
+    mirrorWorld = new char[rowsPlus][colsPlus];
     for(int h = 0; h < loops; h++){
+
       for(int i = 1; i <= rows; i++) {
         for(int j = 1; j <= cols; j++) {
           int x = glider(i,j);
@@ -37,13 +50,16 @@ public class World{
             mirrorWorld[i][j] = ' ';
           if(x == 3)
             mirrorWorld[i][j] = '*';
-          // System.out.println(x);
+          System.out.println("Spot at: " + i + ", " + j + ": " +
+            world[i][j] + " -> " + x + " -> " + mirrorWorld[i][j]);
         }
       }
-      world = mirrorWorld;
+
+
       drawWorld();
+      world = mirrorWorld;
       try{
-        Thread.sleep(500);
+        Thread.sleep(1000);
       } catch (InterruptedException e) {
         e.printStackTrace();
       }
@@ -53,7 +69,7 @@ public class World{
   private void fillRandom(){
     for(int i = 1; i <= rows; i++) {
       for(int j = 1; j <= cols; j++) {
-        if((int)(Math.random() * 20) == 0){
+        if((int)(Math.random() * randomFactor) == 0){
           world[i][j] = '*';
           // System.out.println("Filling point " + i + ", " + j);
         }
@@ -78,15 +94,15 @@ public class World{
     // System.out.println("Grid:\n_________");
     for(int i = x - 1; i <= x + 1; i++) {
       for(int j = y - 1; j <= y + 1; j++){
-        // System.out.print(world[i][j]);
         if(world[i][j] == '*')
           n++;
+          System.out.println(i + ", " + j + " -> " + world[i][j]);
       }
       // System.out.println();
     }
-    if(world[x][y] == '*')
-      n--;
+    // if(world[x][y] == '*')
     // System.out.println("Point " + x + ", " + y + " has " + n + " neighbors.");
-    return n;
+    if(world[x][y] == '*') return n - 1;
+    else return n;
   }
 }
